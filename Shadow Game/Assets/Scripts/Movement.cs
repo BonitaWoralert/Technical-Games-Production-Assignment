ea765daf65f3278856e7d2870e8b7b7edcb2b0ba
Modget <aaronthorpe80@gmail.com>
2023-01-06 12:27:49 +0000

    Furry Tail

--- Shadow Game/Assets/Scripts/Movement.cs
+++ Shadow Game/Assets/Scripts/Movement.cs
@@ -1,17 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D rb;
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
-    public bool isGrounded = true;
-    public LayerMask groundLayers;
-    public Transform groundCheck;
-    private ShadowForm shadowForm;
+    public float dashSpace;
 
+    public bool isGrounded = true;
+
+    public LayerMask groundLayers;
+
+    public Transform groundCheck;
+    public GameObject leftCheck;
+    public GameObject rightCheck;
+
+    public float maxDashTimer;
+
+    public float dashTimer;
+    Rigidbody2D rb;
+    private ShadowForm shadowForm;
+    float horizontalMovement;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +33,53 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        PlayerMove();
+        ShadowMove();
+        if (dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+        }
     }
 
-    void Move()
+    private void ShadowMove()
     {
+        if (!shadowForm.isInShadowForm) { return; }
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.35f, groundLayers);
 
         float horizontalMovement = Input.GetAxis("Horizontal");
+
+        // Get input for horizontal movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Set the velocity of the rigidbody
+        if (Physics2D.Raycast(leftCheck.transform.position, -transform.up, 0.1f) && horizontalMovement < 0)
+        {
+            Debug.Log("Moving Left");
+            rb.velocity = new Vector2(moveHorizontal * moveSpeed, rb.velocity.y);
+        }
+        else if (!Physics2D.Raycast(leftCheck.transform.position, -transform.up, 0.1f) && horizontalMovement < 0)
+        {
+            Debug.Log("Can't Move Left");
+            rb.velocity = Vector2.zero;
+        }
+        if (Physics2D.Raycast(rightCheck.transform.position, -transform.up, 0.1f) && horizontalMovement > 0)
+        {
+            Debug.Log("Moving Right");
+            rb.velocity = new Vector2(moveHorizontal * moveSpeed, rb.velocity.y);
+        }
+        else if (!Physics2D.Raycast(rightCheck.transform.position, -transform.up, 0.1f) && horizontalMovement > 0)
+        {
+            Debug.Log("Can't Move Right");
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void PlayerMove()
+    {
+        if (shadowForm.isInShadowForm) { return; }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.35f, groundLayers);
+
+        horizontalMovement = Input.GetAxis("Horizontal");
 
         // Get input for horizontal movement
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -43,5 +93,33 @@ public class Movement : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
         }
+
+        if (dashTimer > 0)
+        {
+            Move();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Dash(dashSpace);
+        }
+    }
+
+    private void Move()
+    {
+        if (horizontalMovement < 0)
+        {
+            rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, rb.velocity.x - dashSpace, dashTimer), 0);
+        }
+        if (horizontalMovement > 0)
+        {
+            rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, rb.velocity.x + dashSpace, dashTimer), 0);
+        }
+    }
+
+    void Dash(float dashSpace)
+    {
+        Debug.Log("Dashing (through the snow)");
+        dashTimer = maxDashTimer;
     }
 }
--- Shadow Game/Assets/Scripts/ShadowForm.cs
+++ Shadow Game/Assets/Scripts/ShadowForm.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ShadowForm : MonoBehaviour
 {
     public bool isInShadowForm;
     public bool isInDarkness;
+    public Light2D shadowLight;
 
+    private Sprite playerSprite;
+    private PlayerStats stats;
+    private Animator animator;
+    private Movement movement;
     // Start is called before the first frame update
     void Start()
     {
         isInShadowForm = false;
+        playerSprite = GetComponent<Sprite>();
+        stats = GetComponent<PlayerStats>();
+        animator = GetComponent<Animator>();
+        movement = GetComponent<Movement>();
     }
 
     // Update is called once per frame
@@ -23,20 +33,35 @@ public class ShadowForm : MonoBehaviour
 
     void Change()
     {
-        if (Input.GetKeyDown(KeyCode.F) && isInDarkness)
+        shadowLight.enabled = isInShadowForm;
+
+        if (Input.GetKeyDown(KeyCode.F) && isInDarkness && movement.isGrounded)
         {
-            isInShadowForm = !isInShadowForm;
+            if (isInShadowForm)
+            {
+                isInShadowForm = !isInShadowForm;
+            }
+            else if (!isInShadowForm && stats.shadowEnergy > 0f)
+            {
+                isInShadowForm = !isInShadowForm;
+            }
         }
+
+        if (stats.shadowEnergy <= 0)
+        {
+            isInShadowForm = false;
+        }
+
         //IS in Shadow Form
         if (isInShadowForm)
         {
-
+            animator.SetBool("isPlayer", false);
         }
 
         //ISNT in Shadow Form
         if (!isInShadowForm)
         {
-
+            animator.SetBool("isPlayer", true);
         }
     }
 }
