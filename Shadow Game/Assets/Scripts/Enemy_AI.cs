1b3453d154f6954f5b23734ebec06fdab9097a2e
Nicholas Deli <92155926+NFDELI@users.noreply.github.com>
2023-01-06 12:27:41 +0000

    Added Enemy Prefab

--- Shadow Game/Assets/Scripts/Enemy_AI.cs
+++ Shadow Game/Assets/Scripts/Enemy_AI.cs
@@ -78,6 +78,7 @@ public class Enemy_AI : MonoBehaviour
         suspiciousFillUpSpeed = 30f;
         suspiciousDrainSpeed = 15f;
         suspiciousThreshold = 50f;
+        suspiciousValueMax = 120f;
 
 
         chaseThreshold = 100f;
