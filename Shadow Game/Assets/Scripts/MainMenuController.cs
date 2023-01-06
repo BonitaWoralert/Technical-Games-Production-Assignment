61251bf124f1b39caf64a1743d9ebc1bb1ee9b0b
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2023-01-06 10:51:14 +0000

    buttons functional, added submenus

--- Shadow Game/Assets/Scripts/MainMenuController.cs
+++ Shadow Game/Assets/Scripts/MainMenuController.cs
@@ -5,27 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject controlsMenu;
+    public GameObject creditsMenu;
+
     public void StartGame()
     {
-
+        SceneManager.LoadScene("Aarons Scene");
     }
     
     public void Controls()
     {
-
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
     }
 
     public void Settings()
     {
-
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
     }
 
     public void Credits()
     {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
 
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        controlsMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
     }
 
     public void Exit()
     {
+        Application.Quit();
     }
 }
