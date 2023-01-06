484e46a41b458f3444464d53b84771aba5fe0cc3
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2023-01-06 10:19:29 +0000

    Base Main Menu

---
+++ Shadow Game/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+    
+    public void Controls()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Exit()
+    {
+    }
+}
