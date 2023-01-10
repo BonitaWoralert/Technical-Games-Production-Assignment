9cbe770bc0322af67d9b58fcd2e95fd7973962b4
Adam Shaw <92310540+AdamShaw01@users.noreply.github.com>
2023-01-10 16:07:43 +0000

    Tutorial continued

--- Shadow Game/Assets/Scripts/Collision.cs
+++ Shadow Game/Assets/Scripts/Collision.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
     private CameraController bob;
+    private bool noCollision = true;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (noCollision == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                Debug.Log("Hi");
+                SceneManager.LoadScene("Tutorial");
+            }
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -19,6 +28,7 @@ public class Collision : MonoBehaviour
             Debug.Log("Dead");
             bob = GameObject.FindObjectOfType<CameraController>();
             bob.enabled = false;
+            noCollision = false;
         }
     }
 }
