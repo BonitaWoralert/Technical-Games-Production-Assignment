c7491e73a32bf19a6332fde6b1023a540529f429
Evin Oxley <91544713+EvinIkuyama@users.noreply.github.com>
2023-01-13 11:07:29 +0000

    Updated Anim + Load Screen Start

--- Shadow Game/Assets/Scripts/Puzzle Elements/Button/Button.cs
+++ Shadow Game/Assets/Scripts/Puzzle Elements/Button/Button.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Button : BaseSwitch
 {
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         _state = true;
+        animator.SetBool("isActive", true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _state = false;
+        animator.SetBool("isActive", false);
     }
 }
