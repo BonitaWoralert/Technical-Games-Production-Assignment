fdf1502e16c4ed7e3a10971f1948d5a34f52807a
Adam Shaw <92310540+AdamShaw01@users.noreply.github.com>
2023-01-24 15:18:17 +0000

    Universal level switch script part 1
    Making a universal script for all level transitions so that the player spawns where they should on each level depending on their entry.

--- Shadow Game/Assets/Scripts/LevelLoader.cs
+++ Shadow Game/Assets/Scripts/LevelLoader.cs
@@ -10,9 +10,18 @@ public class LevelLoader : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && gameObject.name == "Next Level Trigger")
         {
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+            PlayerPrefs.SetInt("Spawn", 2);
+            PlayerPrefs.Save();
+        }
+
+        if (other.gameObject.tag == "Player" && gameObject.name == "Previous Level Trigger")
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+            PlayerPrefs.SetInt("Spawn", 1);
+            PlayerPrefs.Save();
         }
     }
 
--- Shadow Game/Assets/Scripts/Player_Spawn.cs
+++ Shadow Game/Assets/Scripts/Player_Spawn.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class Player_Spawn : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 objpos;
 
     void Start()
     {
         if (PlayerPrefs.GetInt("Spawn") == 1)
         {
-            //Set the player position the the end of the level
-            transform.position = new Vector3(43, 0.84f, 0);
+            //Set the player position to the end of the level
+            objpos = new Vector3(GameObject.Find("Next Level Trigger").transform.position.x - 2, 
+                GameObject.Find("Next Level Trigger").transform.position.y, GameObject.Find("Next Level Trigger").transform.position.z);
+            transform.position = objpos;
             //Reset player pref
             PlayerPrefs.SetInt("Spawn", 0);
             PlayerPrefs.Save();
         }
-    }
-    public void LevelEntry()
-    {
 
+        if (PlayerPrefs.GetInt("Spawn") == 2)
+        {
+            //Set the player position to the start of the level
+            objpos = new Vector3(GameObject.Find("Previous Level Trigger").transform.position.x + 20,
+                GameObject.Find("Next Level Trigger").transform.position.y, GameObject.Find("Next Level Trigger").transform.position.z);
+            transform.position = objpos;
+            //Reset player pref
+            PlayerPrefs.SetInt("Spawn", 0);
+            PlayerPrefs.Save();
+        }
     }
 }
