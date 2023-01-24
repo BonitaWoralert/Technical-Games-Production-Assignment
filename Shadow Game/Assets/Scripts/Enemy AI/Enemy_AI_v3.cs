1c317ce489f923a84d4e38aba9e8018c5e542591
Nicholas Deli <92155926+NFDELI@users.noreply.github.com>
2023-01-24 13:44:39 +0000

    Made Pathfinding Work

--- Shadow Game/Assets/Scripts/Enemy AI/Enemy_AI_v3.cs
+++ Shadow Game/Assets/Scripts/Enemy AI/Enemy_AI_v3.cs
@@ -11,10 +11,16 @@ public class Enemy_AI_v3 : MonoBehaviour
     [SerializeField] private float speed; //200f //400f
     [SerializeField] private float nextWayPointDistance; //1f
     [SerializeField] private float pathUpdateSpeed; //0.5f
+    [SerializeField] private float jumpStrength;
+    [SerializeField] private float jumpThreshold;
+    [SerializeField] private float jumpTimer;
+    [SerializeField] private float defautJumpTimer;
+    [SerializeField] private bool isJumping;
 
     private Path path;
     private int currentWayPoint = 0;
     private bool reachedEndOfPath = false;
+    //private Vector2 direction;
 
     Seeker seeker;
     Rigidbody2D rb;
@@ -25,18 +31,21 @@ public class Enemy_AI_v3 : MonoBehaviour
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
+        jumpTimer = defautJumpTimer;
 
-        InvokeRepeating("UpdatePath", 0f, pathUpdateSpeed);
+        InvokeRepeating("UpdatePath", 1f, pathUpdateSpeed);
     }
 
     void UpdatePath()
     {
+        //Make sure that the enemy is not jumping.
         //Make sure that the seeker is NOT currently calculating a new path.
-        if(seeker.IsDone())
+        if (seeker.IsDone())
         {
             //Generate new Path.
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
+
     }
 
     void OnPathComplete(Path p)
@@ -46,6 +55,15 @@ public class Enemy_AI_v3 : MonoBehaviour
             path = p;
             currentWayPoint = 0;
         }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("q"))
+        {
+            Jump();
+        }
+        JumpTimerUpdate();
     }
 
     void FixedUpdate()
@@ -66,11 +84,24 @@ public class Enemy_AI_v3 : MonoBehaviour
         }
 
         Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
-        Vector2 moveForce = direction * speed * Time.deltaTime;
+        Vector2 moveForce = direction * speed * Time.fixedDeltaTime;
+        Vector2 moveForceX = new Vector2(direction.x * speed * Time.fixedDeltaTime, 0f);
         Vector2 moveForceSecond = direction * speed;
 
-        rb.AddForce(moveForce);
-        //rb.velocity = moveForceSecond;
+        if(isJumping == true)
+        {
+            rb.AddForce(moveForceX);
+        }
+        else
+        {
+            //Moving Left or Right.
+            rb.AddForce(moveForce);
+        }
+
+        if(moveForce.y > jumpThreshold)
+        {
+            Jump();
+        }
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
 
@@ -88,4 +119,27 @@ public class Enemy_AI_v3 : MonoBehaviour
             enemySprite.localScale = new Vector3(-5f, 5f, 5f);
         }
     }
+
+    private void Jump()
+    {
+        if(isJumping == false)
+        {
+            rb.velocity += Vector2.up * jumpStrength;
+            isJumping = true;
+        }
+    }
+
+    private void JumpTimerUpdate()
+    {
+        if(isJumping == true && jumpTimer > 0)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpTimer = defautJumpTimer;
+        }
+    }
+
 }
