6e35564a76ce6ae2be04499743cd2fe9d87d1c5e
Adam Shaw <92310540+AdamShaw01@users.noreply.github.com>
2023-01-27 13:17:51 +0000

    Smooth level transitions
    Now when the player transitions, their X axis stays the same rather than them always facing right.

--- Shadow Game/Assets/Scripts/Player_Spawn.cs
+++ Shadow Game/Assets/Scripts/Player_Spawn.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Player_Spawn : MonoBehaviour
 {
-    GameObject player;
     private Vector3 objpos;
 
     void Start()
@@ -15,19 +14,35 @@ public class Player_Spawn : MonoBehaviour
             objpos = new Vector3(GameObject.Find("Next Level Trigger").transform.position.x - 2, 
                 GameObject.Find("Next Level Trigger").transform.position.y, GameObject.Find("Next Level Trigger").transform.position.z);
             transform.position = objpos;
+
+            //Sets the rotation of the player
+            if (PlayerPrefs.GetInt("SpawnFlip") == 1)
+                GetComponent<SpriteRenderer>().flipX = true;
+            else
+                GetComponent<SpriteRenderer>().flipX = false;
+
             //Reset player pref
             PlayerPrefs.SetInt("Spawn", 0);
+            PlayerPrefs.SetInt("SpawnFlip", 0);
             PlayerPrefs.Save();
         }
 
         if (PlayerPrefs.GetInt("Spawn") == 2)
         {
-            //Set the player position to the start of the level
-            objpos = new Vector3(GameObject.Find("Previous Level Trigger").transform.position.x + 20,
-                GameObject.Find("Next Level Trigger").transform.position.y, GameObject.Find("Next Level Trigger").transform.position.z);
+            //Set the player position to the end of the level
+            objpos = new Vector3(GameObject.Find("Previous Level Trigger").transform.position.x + 2 ,
+                GameObject.Find("Previous Level Trigger").transform.position.y, GameObject.Find("Previous Level Trigger").transform.position.z);
             transform.position = objpos;
+
+            //Sets the rotation of the player
+            if (PlayerPrefs.GetInt("SpawnFlip") == 1)
+                GetComponent<SpriteRenderer>().flipX = true;
+            else
+                GetComponent<SpriteRenderer>().flipX = false;
+
             //Reset player pref
             PlayerPrefs.SetInt("Spawn", 0);
+            PlayerPrefs.SetInt("SpawnFlip", 0);
             PlayerPrefs.Save();
         }
     }
