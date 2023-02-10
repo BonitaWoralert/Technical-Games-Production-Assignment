05fa5d3827ac3b8bc46933dea3f146e7dd4966b5
Evin Oxley <91544713+EvinIkuyama@users.noreply.github.com>
2023-02-10 12:32:57 +0000

    Admin Commands Removed

--- Shadow Game/Assets/Scripts/Player/PlayerStats.cs
+++ Shadow Game/Assets/Scripts/Player/PlayerStats.cs
@@ -26,20 +26,7 @@ public class PlayerStats : MonoBehaviour
     void Update()
     {
         ChangeShadowEnergy(0);
-        AdminCommands();
 
-    }
-
-    private void AdminCommands()
-    {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            ChangeShadowEnergy(5f);
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            ChangeShadowEnergy(100f);
-        }
     }
 
     private void ChangeShadowEnergy(float change)
